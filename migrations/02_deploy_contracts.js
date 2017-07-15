c9068cd14c34640a44d3832248467c2caadf5ebc
Artem Demo <artemdemo@gmail.com>
2017-07-15 14:51:50 +0300

    Simplified

--- migrations/02_deploy_contracts.js
+++ migrations/02_deploy_contracts.js
@@ -1,7 +1,5 @@
-const TweetRegistry = artifacts.require("./TweetRegistry.sol");
 const TweetAccount = artifacts.require("./MetaCoin.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(TweetRegistry);
     deployer.deploy(TweetAccount);
 };
